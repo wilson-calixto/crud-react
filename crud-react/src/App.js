a23a8b809fc30e377fc7162ee6397fc84a815e49
wilson.calixto <wilson.calixto@grupoicts.com.br>
2020-04-16 16:21:49 -0400

    adicionando a tabela

---
+++ crud-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CustomDataTable from './pages/CustomDataTable';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CustomDataTable />
+    </div>
+  );
+}
+
+export default App;
